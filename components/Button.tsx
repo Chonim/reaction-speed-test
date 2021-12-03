cc4783efd8bbd1a9ee07c1284d6de74fc0a6e224
Chiho <chiho1jjan@naver.com>
2021-12-03 18:48:05 +0900

    feat: implement main page

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+interface Props {
+  
+}
+
+const Button = (props: Props) => {
+  return (
+    <StyledButton>
+      hoho
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled.button`
+  color: red;
+`
+
+export default Button
