4948e07d17b57580610604403f26d62a87f3f39b
Chiho <chiho1jjan@naver.com>
2021-12-09 22:45:05 +0900

    feat: implement base layout

---
+++ components/common/BaseLayout.tsx
@@ -0,0 +1,43 @@
+import React, { Children } from 'react'
+import styled from "@emotion/styled";
+import { palette } from "@styles/theme";
+
+type BaseLayoutProps = {
+  pageTitle: string,
+  handleBackgroundClick: () => void
+  children: React.ReactNode | React.ReactNodeArray
+}
+
+const BaseLayout = ({
+  pageTitle,
+  handleBackgroundClick,
+  children,
+}: BaseLayoutProps) => (
+  <Container onClick={handleBackgroundClick}>
+    <PageTitle>{pageTitle}</PageTitle>
+    {children}
+    <AdArea />
+  </Container>
+);
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const PageTitle = styled.h1`
+  padding-top: 1rem;
+  color: ${palette.green};
+  font-weight: 600;
+  font-size: 1.25rem;
+  text-align: center;
+`;
+
+const AdArea = styled.div`
+  width: 100vw;
+  height: 4rem;
+  background-color: #700965;
+  position: fixed;
+  bottom: 0;
+`;
+
+export default BaseLayout
